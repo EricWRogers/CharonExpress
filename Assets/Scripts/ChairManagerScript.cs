7908d38f7a9e1940d49fef159f1d98334ee13485
Sawyer Grimm <47121275+sawyergrimm@users.noreply.github.com>
2026-09-01 17:03:18 -0500

    Added placeholder spheres to represent ghosts. For each chair, they spawn in, are assigned a placeholder task, despawn after a 'random' time, and respawn after 'random' time.

---
+++ Assets/Scripts/ChairManagerScript.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEngine;
+using UnityEngine.LowLevelPhysics2D;
+
+public class ChairManager : MonoBehaviour
+{
+    public bool testBool;
+    public GameObject[] chairs;
+    [SerializeField] GameObject[] ghosts;
+
+    //Keeps track of how many ghosts have been through in total
+    public int customerTotal = 0;
+    //customerTotal modulo by the length of chairs
+    int customerModulo;
+
+    //Sample list of names that is assigned to the chair/ghost
+    String[] names = {"Sawyer", "Zek", "Cooper", "John", "Joe"};
+    String[] tasks = {"Hit button 1", "Hit button 2", "Hit button 3"};
+
+
+    void Start()
+    {
+        chairs = GameObject.FindGameObjectsWithTag("Chair");
+
+        //Hides the ghosts. They will be toggled on when appropriate.
+        foreach (GameObject ghost in ghosts)
+        {
+            ghost.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+    }
+
+    public void AssignGhost(GameObject chair)
+    {
+        customerTotal++;
+        ChairScript chairScript = chair.GetComponent<ChairScript>();
+        string taskToAssign = tasks[UnityEngine.Random.Range(0, tasks.Length)];
+        chairScript.task = taskToAssign;
+        Debug.Log("Assigned the task" + taskToAssign + " count: " + customerTotal);
+    }
+}
--- Assets/Scripts/ChairScript.cs
+++ Assets/Scripts/ChairScript.cs
@@ -1,17 +1,55 @@
 using System;
 using UnityEngine;
 
+//The goal of this script is to serve as the "container" for the chair and store the data that pertains to that chair and that chair only
+//For example, managing the timer on how long the ghost will be present, what task it will have, etc.
+//It is easier to have it managed by a constant chair script instead of a ghost script that moves around the scene.
+
+
 public class ChairScript : MonoBehaviour
 {
-    String name = "";
+    public GameObject chairManager;
+    ChairManager chairManagerScript;
+    public String ghostName = "";
+    public bool ghostActive = false;
+    public String task = "";
+    public int customerTimer;
+    public int cooldownTimer;
+    public GameObject ghostObject;
     void Start()
     {
-
+        cooldownTimer = UnityEngine.Random.Range(100, 500);
+        customerTimer = 0;
+        ghostObject = transform.GetChild(1).gameObject;
+        chairManager = GameObject.Find("ChairManager");
+        chairManagerScript = chairManager.GetComponent<ChairManager>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (customerTimer > 0)
+        {
+            customerTimer--;
+        } 
+        else if (cooldownTimer > 0)
+        {
+            cooldownTimer--;
+        } 
+        else if (cooldownTimer == 0 && ghostActive == false)
+        {
+            ghostActive = true;
+            ghostObject.SetActive(true);
+            Debug.Log("I GAVE IT LIFE");
+            chairManagerScript.AssignGhost(gameObject);
+            customerTimer = UnityEngine.Random.Range(500, 2000);
+        }
+        else if (customerTimer == 0 && ghostActive == true) 
+        {
+            ghostActive = false;
+            ghostObject.SetActive(false);
+            Debug.Log("I KILLED IT");
+            cooldownTimer = UnityEngine.Random.Range(1000,500);
+        } 
     }
 }
