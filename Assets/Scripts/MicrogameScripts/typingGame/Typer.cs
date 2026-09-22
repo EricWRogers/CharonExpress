50dc4bc7befd9fe9078dddc12b3f7caaa94a673e
CooperCantrell <coopercantrell56@gmail.com>
2026-09-22 16:42:07 -0500

    typing game but no word bank

--- Assets/Scripts/MicrogameScripts/typingGame/Typer.cs
+++ Assets/Scripts/MicrogameScripts/typingGame/Typer.cs
@@ -1,26 +1,85 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using TMPro;
 
 public class Typer : MonoBehaviour
 {
-
-    public Text wordOutput;
+    public GameObject typingPanel;
+    public TMP_Text wordOutput;
 
     private string remainingWord = string.Empty;
-    private string currentWord = "Katabasis";
+    private string currentWord = "testing a sentence in the typing game";
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
         SetCurrentWord();
     }
 
-    private void SetCurrentWord(){
+    private void SetCurrentWord()
+    {
         //Get bank word
+        SetRemainingWord(currentWord);
+    }
+
+    private void SetRemainingWord(string newWord)
+    {
+        remainingWord = newWord;
+        wordOutput.text = remainingWord;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        
+        CheckInput();
+    }
+
+    private void CheckInput()
+    {
+        if (Input.anyKeyDown)
+        {
+            string keysPressed = Input.inputString;
+
+            if (keysPressed.Length == 1)
+            {
+                EnterLetter(keysPressed);
+            }
+        }
+    }
+
+    private void EnterLetter(string TypedLetter)
+    {
+        if (IsCorrectLeetter(TypedLetter))
+        {
+            RenoveLetter();
+
+            if (WordComplete())
+            {
+                typingPanel.SetActive(false);
+                Time.timeScale = 1f;
+            }
+        }
+    }
+
+    private bool IsCorrectLeetter(string letter)
+    {
+        return remainingWord.IndexOf(letter) == 0;
+    }
+
+    private void RenoveLetter()
+    {
+        string newWord = remainingWord.Remove(0, 1);
+        SetRemainingWord(newWord);
+    }
+
+    private bool WordComplete()
+    {
+        return remainingWord.Length == 0;
+    }
+    public void StartGame()
+    {
+        SetCurrentWord();
+        typingPanel.SetActive(true);
+        Time.timeScale = 0f;
     }
 }
