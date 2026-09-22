9c93b1211746805ec2b3055a5d188ef4df36849c
CooperCantrell <coopercantrell56@gmail.com>
2026-09-22 16:27:20 -0500

    start of typing game

---
+++ Assets/Scripts/MicrogameScripts/typingGame/Typer.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Typer : MonoBehaviour
+{
+
+    public Text wordOutput;
+
+    private string remainingWord = string.Empty;
+    private string currentWord = "Katabasis";
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        SetCurrentWord();
+    }
+
+    private void SetCurrentWord(){
+        //Get bank word
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
